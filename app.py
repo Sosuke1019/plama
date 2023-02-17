ed04f3b0240fe891f60c0cd3ddb43d9595b0b961
Sosuke1019 <kato.052wazime@gmail.com>
2023-02-17 11:19:06 +0900

    fix index.html & app.py

--- app.py
+++ app.py
@@ -1,35 +1,15 @@
 from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from models.models import user
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://///home/sosuke/CODEGYM_FinalProject/plama/db/plama.db"
-db = SQLAlchemy(app)
-
-# class user(db.Model):
-#     __tablename__ = "user"
-#     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-#     name = Column(Text, nullable=False)
-#     room_number = Column(Text, nullable=False)
-#     hash = Column(VARCHAR, nullable=False)
-
-
-
-# class product(db.Model):
-#     __tablename__ = 'product'
-#     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-#     user_id  = Column(Integer, ForeignKey("user.id", nullable=False))
-#     title = Column(Text, nullable=False)
-#     body  = Column(Text, nullable=False)
-#     picture_path  = Column(Text, nullable=False)
-#     date = Column(DateTime, default=datetime.now(), nullable=False)
 
 
 @app.route('/')
 def index():
     """Show a list of products"""
-    return render_template("index.html")
+    users = user.query.all()
+    return render_template("index.html", users=users)
 
 @app.route('/register')
 def register():
